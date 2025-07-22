cee43f0606c25bd320bc1dd54b1dd92e80a65fd9
Ana Márquez <AnaMarquez@Rubidexs-Mac-mini-2.local>
2025-07-22 16:40:27 -0600

    feat:add lesson2 files

---
+++ Lesson 2/main.js
@@ -0,0 +1,12 @@
+//AND
+let valida1 = true && true;
+console.log("true && true" + valida1);
+
+valida1 = true && false;
+console.log("true && false" + valida1);
+
+valida1 = false && true;
+console.log("false && true" + valida1);
+
+valida1 = false && false;
+console.log("false && false" + valida1);
