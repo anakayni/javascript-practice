98ea328c542be21ff0e29306e2ade671fb4fba47
Ana Márquez <AnaMarquez@Rubidexs-Mac-mini-2.local>
2025-07-24 16:54:00 -0600

    feat(condicionales.js):add condicionales.js

---
+++ Lesson 2/condicionales.js
@@ -0,0 +1,21 @@
+let unNumero = 3;
+
+if (unNumero == 5){
+    console.log ("Vemos el mensaje porque es true");
+}
+
+if (unNumero != 5){
+    console.log("No vemos el mensaje porque es false");
+}
+
+if((unNumero >= 1 && unNumero <= 4) || (unNumero >= 7)){
+    console.log("El numero esta en el rango");
+}
+
+let validar = unNumero > 5;
+if (!validar){
+    console.log("El numero es mayor a 5");
+}
+
+
+
--- Lesson 2/main.js
+++ Lesson 2/main.js
@@ -10,3 +10,28 @@ console.log("false && true" + valida1);
 
 valida1 = false && false;
 console.log("false && false" + valida1);
+
+
+//OR
+valida1 = true || true;
+console.log("true || true: " + valida1);
+
+valida1 = true || false;
+console.log("true || false: " + valida1);
+
+valida1 = false || true;
+console.log("false || true: " + valida1);
+
+valida1 = false || false;
+console.log("false || false: " + valida1);
+
+
+//NOT
+valida1 = !true;
+console.log("!true: " + valida1);
+
+valida1 = !false;
+console.log("!false: " + valida1);
+
+
+
